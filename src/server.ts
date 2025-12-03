6c3da0d86047a3a043b6c11ec2e7b4f26c5533c5
tahsin-dev225 <ahmedtasin225@gmail.com>
2025-12-03 20:57:47 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,61 @@
+import express, { Request, Response } from 'express'
+import {Pool} from 'pg'
+import dotenv from "dotenv";
+import path from 'path';
+
+dotenv.config({path : path.join(process.cwd(), '.env')})
+const app = express()
+const port = 5000
+
+const pool = new Pool({
+  connectionString : `${process.env.CONNECTION_STR}`
+})
+
+const initDB = async () =>{
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    age INT,
+    phone VARCHAR(15),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `);
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS todos (
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES users(id) ON DELETE CASCATE,
+      title VARCHAR(200) NOT NULL,
+      description TEXT,
+      completed BOOLEAN DEFAULT false,
+      due_date DATE,
+      created_at TIMESTEMP DEFAULT NOW(),
+      updated_at TIMESTEMP DEFAULT NOW ()
+      )
+      `)
+}
+
+initDB()
+
+app.use(express.json())
+// app.use(express.urlencoded())
+
+app.get('/', (req : Request, res : Response) => {
+  res.send('Hello World!!!')
+})
+
+app.post('/', (req : Request, res : Response) => {
+  console.log(req.body);
+  res.status(201).json({
+    success : true,
+    message : "API is working"
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
